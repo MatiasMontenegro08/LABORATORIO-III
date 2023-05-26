2b911f841d8b6d55e1f8b2faf3778f4631302491
unknown <matiasmontenegro0810@gmail.com>
2023-05-26 19:57:55 -0300

    mayo

---
+++ JS/Clase24/5/funciones.js
@@ -0,0 +1,15 @@
+var a = prompt("Ingrese un número!")
+
+function pnc(a){
+    if (a==0){
+      alert("Es Cero")
+    }else{
+      if (a>0){
+      alert("Positivo")
+    }else{
+      alert("Negativo")
+    }
+  }
+  }
+
+  pnc(a);
